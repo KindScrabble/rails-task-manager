10a976e8fdaacdc05c02ec824808555f4ea91d97
Joana Reis <joanarfreis@gmail.com>
2018-05-22 14:58:26 +0100

    almost completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   get "tasks/new", to: "tasks#new"
    get "tasks", to: "tasks#index"
-   get "tasks/:id", to: "tasks#show"
+   get "tasks/:id", to: "tasks#show", as: "tasks_id"
 end
