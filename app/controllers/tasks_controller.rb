4134e1413e0d47dfd14919f866431152cacff592
Joana Reis <joanarfreis@gmail.com>
2018-05-22 16:04:31 +0100

    Rails Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,5 +9,31 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
    get "tasks/new", to: "tasks#new"
    get "tasks", to: "tasks#index"
    get "tasks/:id", to: "tasks#show", as: "tasks_id"
+  post "tasks", to: "tasks#create"
+   get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+delete "tasks/:id", to: "tasks#destroy", as: "task_delete"
+
 end
